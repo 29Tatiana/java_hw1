53acd407b0d60512ea163821d98ba89254cee221
29Tatiana <amiravob@mail.ru>
2023-01-29 20:47:03 +0300

    sem2

---
+++ src/main/java/sem2_hw/Main.java
@@ -0,0 +1,42 @@
+package sem2_hw;
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ex2();
+        ex1();
+    }
+
+    private static void ex1() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = input.next();
+        input.close();
+
+        String new_str = new StringBuilder(str).reverse().toString();
+
+        if (new_str.equals(str)) {
+            System.out.printf("Строка %s является палиндромом", str);
+        } else {
+            System.out.printf("Строка %s не является палиндромом", str);
+        }
+    }
+
+    private static void ex2() {
+        writeFile();
+    }
+
+    private static void writeFile() {
+        String testString = "TEST".repeat(100);
+        try (PrintWriter pw = new PrintWriter("src/main/resources/text.txt")) {
+            pw.write(testString);
+            pw.close();
+            System.out.println("Запись добавлена");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Запись не работает");
+        }
+
+    }
+}
